a99b0981ce931c8a49fa3269cbaa4e8c9841e905
Rasmus Thomsen <oss@cogitri.dev>
2021-12-01 23:53:26 +0100

    Ignore unexpected arguments

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, ... }:
 let
   callPackage = pkgs.callPackage;
   stdenv = pkgs.stdenv;
